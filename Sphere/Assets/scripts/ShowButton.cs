c2a926cd5bfbbad532ddba731e57e0c2af70d10c
versave <vszhivkov@gmail.com>
2020-01-16 16:07:12 +0200

    Add in game button ui

---
+++ Sphere/Assets/scripts/ShowButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowButton : MonoBehaviour
+{
+    GameObject button;
+
+    private void Start() {
+        button = GameObject.FindGameObjectWithTag("button");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")) {
+            Debug.Log(1);
+
+            button.SetActive(false);
+        } else if(Input.GetKeyUp("space")) {
+            Debug.Log(2);
+
+            button.SetActive(true);
+        }    
+    }
+}
