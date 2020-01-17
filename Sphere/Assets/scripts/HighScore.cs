4bd044d44b1148bf3edddcea72018edef2e8f759
versave <vszhivkov@gmail.com>
2020-01-17 10:49:49 +0200

    Add highscore scene

---
+++ Sphere/Assets/scripts/HighScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    Text field;
+    public float highScore = 0;
+
+    private void incScore() {
+        highScore += 0.05f;
+    }
+
+    void Start() {
+        field = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Scale")) {
+            incScore();
+
+            field.text = Mathf.Floor(highScore).ToString();
+        }       
+    }
+}
